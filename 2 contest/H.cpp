ee9613e523b9964212118d240ab3d3e745df44eb
Bravvel <120821657+Bravvel@users.noreply.github.com>
2025-01-25 22:11:08 +0300

    Create H.cpp

---
+++ 2 contest/H.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int x;
+ 
+int main() {
+	cin >> x;
+	if(x <= 5){
+		cout << 1;
+		return 0;
+	} else{
+		if(x % 5 == 0) cout << x / 5;
+		else cout << x / 5 + 1;
+	}
+	
+	return 0;
+}
