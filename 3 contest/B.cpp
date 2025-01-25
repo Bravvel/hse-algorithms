3038afaff07e0ad77a23856d02328fccef7b527c
Bravvel <120821657+Bravvel@users.noreply.github.com>
2025-01-25 22:13:08 +0300

    Create B.cpp

---
+++ 3 contest/B.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+ 
+bool isPalindrom(string s){
+	for(int i = 0; i < s.size()/2; i++){
+		if(s[i] != s[s.size()-i-1]) return false;
+	}
+	return true;
+}
+ 
+int main() {
+	int T;
+	cin >> T;
+	while(T--){
+		string s;
+		cin >> s;
+		if(!isPalindrom(s)){
+			cout << s << endl;
+			continue;
+		}
+		bool check = true;
+		int n = s.size();
+		for(int i = 0; i < n - 1; i++){
+			if(s[i] != s[i+1]){
+				check = false;
+				swap(s[i], s[i+1]);
+				cout << s << endl;
+				break;
+			}	
+		}
+		if(check){
+			cout << -1 << endl;
+		}
+	}
+	return 0;
+}
